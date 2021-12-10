f99e2c19d0fcf21486651c2e98bcd5ddabb9ee5e
Pranav Putta <pranav.putta22@gmail.com>
2021-12-09 21:46:04 -0500

    added bert head

--- dataloader.py
+++ dataloader.py
@@ -1,13 +1,15 @@
 from datasets import load_dataset
 from config import CONFIG
 from transformers import AutoTokenizer
+import numpy as np
+
+tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
 
 
 def load(dataset_name=CONFIG.dataset):
     return load_dataset(dataset_name[0])
 
 
-def process_input(inp):
-    tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
-    out = tokenizer(inp, padding='do_not_pad', return_tensors='pt')
-    return out['input_ids'], out['attention_mask']
+def process_input(inp, segment=0):
+    out = tokenizer(inp)
+    return out['input_ids'], segment * np.ones(len(out['token_type_ids']))
--- main.py
+++ main.py
@@ -1,17 +1,54 @@
+import torch
+
 from dataloader import load, process_input
-import pandas as pd
-from config import CONFIG
-from network import ClusterFormer
+from transformers import AutoModel
 
-data = load()
-questions = data['train']['question']
+torch.cuda.empty_cache()
+bert_transformer = AutoModel.from_pretrained('bert-base-uncased')
 
-input_ids, attention_mask = process_input(questions[0])
-net = ClusterFormer()
 
-print(input_ids)
-output = net(input_ids.T)
+def train():
+    # preprocess data
+    data = load()
+    data = data['train'][:100]
+    questions = data['question']
+    questions, question_seg = process_input(questions, segment=0)
 
-print(output)
-print(input_ids.shape)
-print(output.shape)
+    contexts, contexts_seg = [], []
+    for context in data['context_long']:
+        content, content_seg = [], []
+        # concatenate each document into a single content array and separate them by a separator token and segment id
+        content = "".join(context['content'])
+        contexts.append(content)
+
+    contexts, contexts_seg = process_input(contexts, segment=1)
+
+    # break into 256 sized chunks
+    inputs = []
+    stride = 256
+    for j, context in enumerate(contexts):
+        chunks = [context[i:i + 256] for i in range(0, len(context), stride)]
+
+        inp = []
+        for chunk in chunks:
+            sample = (torch.cat((torch.tensor(questions[j]), torch.tensor([102]), torch.tensor(chunk))))
+            if len(sample) > 336:
+                sample = sample[:336]
+            else:
+                sample = torch.cat((sample, torch.zeros(336 - len(sample))))
+            inp.append(sample)
+        inputs.append(torch.vstack(inp).int())
+
+    bert_transformer.train()
+
+    for epoch in range(10):
+        for i, x in enumerate(inputs):
+            torch.cuda.empty_cache()
+            out = bert_transformer(x)
+
+            # slide
+            stride = 248
+            out = [out[:, i:i + stride, :] for i in range(0, out.shape[1])]
+
+
+train()
